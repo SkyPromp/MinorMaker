dd29db0ca776932e088b40ba592da0bc7804d52d
SkyPromp <poppe1max@gmail.com>
2025-11-27 16:42:18 +0100

    fix copy paste issue

--- backend/entities/user.py
+++ backend/entities/user.py
@@ -1,7 +1,7 @@
 from config.dbConfig import db
 
 class User(db.Model):
-    __tablename__ = 'answer'
+    __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
