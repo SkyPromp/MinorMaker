4903a8134e6b416e23bc3902ad137e77b9ef8d3b
SkyPromp <poppe1max@gmail.com>
2025-12-01 16:36:17 +0100

    fix

--- backend/services/userService.py
+++ backend/services/userService.py
@@ -16,6 +16,11 @@ class UserService:
         return self.userRepo.add_user(user).to_json()
 
     def get_users_by_role(self, role):
-        return self.userRepo.get_users_by_role(role).to_json()
+        userJson = []
+
+        for user in self.userRepo.get_users_by_role(role):
+            userJson.append(user.to_json())
+
+        return userJson
 
 
